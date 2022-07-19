7e7c53c9950fab14c61a75f9bf5fa3f9c95170b0
arjav <arjav@192.168.2.51>
2022-07-19 12:08:05 -0400

    pattern

--- JavaClassesPractice/src/javaQuations/PrintLPatter.java
+++ JavaClassesPractice/src/javaQuations/PrintLPatter.java
@@ -3,14 +3,14 @@ package javaQuations;
 public class PrintLPatter {
 	public static void main(String[] args) {
 		
-		solution(100);
+		solution(5);
 		
 	}
 	public static void solution(int N) {
         for(int i=1; i<N; i++){
             System.out.println("L");
         }
-        for(int i=1; i<N; i++){
+        for(int i=1; i<=N; i++){
             System.out.print("L");
         }
     }
